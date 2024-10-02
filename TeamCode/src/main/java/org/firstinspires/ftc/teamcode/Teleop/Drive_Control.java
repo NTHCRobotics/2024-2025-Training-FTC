13229d40608a144c540784235d959f2a881aa4bd
ags3780 <hammerhead12as@icloud.com>
2024-10-02 17:23:39 -0500

    Create Drive_Control.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Drive_Control.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@TeleOp(name="Drive_Control", group="Iterative Opmode")
+
+public class Drive_Control extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+   @Override
+    public void init_loop() {
+
+    }
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    public void Claw() {
+
+    }
+
+    public void Arm() {
+
+    }
+
+    public void Intake() {
+
+    }
+
+
+
+}
